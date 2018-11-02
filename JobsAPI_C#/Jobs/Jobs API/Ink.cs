5c8c77b642f1a22fa6e97d7b7001ef637003d6ab
Chad Stevens <chad.stevens@hp.com>
2018-11-02 16:51:48 -0600

    Changing directory structure and adding readme.md

---
+++ JobsAPI_C#/Jobs/Jobs API/Ink.cs
@@ -0,0 +1,13 @@
+﻿namespace Jobs
+{ 
+    class Ink
+    {
+        public InkCount[] counts { get; set; }
+    }
+
+    class InkCount
+    {
+        public string name { get; set; }
+        public int amountUsed { get; set; }
+    }
+}
---
+++ JobsAPI_C#/Readme.md
@@ -0,0 +1,9 @@
+# PrintOS PrintBeat Jobs C# sample
+
+## General Information
+
+Code was developed in Visual Studio 2015
+
+## How To Run / Program Information
+
+- Import the solution into Visual Studio
